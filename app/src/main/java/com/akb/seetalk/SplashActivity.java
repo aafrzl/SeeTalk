a11644cb604c98109a1751f97ea5b08867746a62
CallMeDidi <diazraflim@gmail.com>
2020-07-11 04:39:24 +0700

    Membuat Splash, Ganti Login Layout

---
+++ app/src/main/java/com/akb/seetalk/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.akb.seetalk;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
